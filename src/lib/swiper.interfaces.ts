060709d26e194d09f73b0ea78d6f45edb3e30581
Rolf Scheuerling <Rolf@Scheuerling.net>
2019-01-14 11:17:25 +0100

    Update swiper.interfaces.ts

--- src/lib/swiper.interfaces.ts
+++ src/lib/swiper.interfaces.ts
@@ -448,7 +448,7 @@ export class SwiperConfig implements SwiperConfigInterface {
 
   // Breakpoints
   public breakpoints?: any;
-  public breakpointsInverse?: any;
+  public breakpointsInverse?: boolean;
 
   // Observer
   public observer?: boolean;
